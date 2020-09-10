2124627cb92ac9a6e18d6e37b3ce1dacf8e285ef
Daniel Fong <daniel.fong.mv@gmail.com>
2020-09-10 14:54:53 -0600

    added basic dashboard input and label

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from '../Dashboard/Dashboard'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Dashboard></Dashboard>
     </div>
   );
 }
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+import React from 'react';
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <label for='location-search'>Search Resorts and Locations</label>
+                <input class='location-search' name='location-search'>
+                </input>
+            </div>
+        )
+    }
+}
