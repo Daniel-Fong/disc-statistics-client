c8fbefcbabfd4ecde320f36658e8c97b04bf9385
Daniel Fong <daniel.fong.mv@gmail.com>
2020-10-02 11:28:48 -0600

    changed routing of public only route in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Dashboard from '../Dashboard/Dashboard';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
-import PublicOnlyRoute from '../PublicOnlyRoute';
+import PublicOnlyRoute from '../PublicOnlyRoute/PublicOnlyRoute';
 import LoginForm from '../LoginForm/LoginForm';
 import RegistrationForm from '../RegistrationForm/RegistrationForm';
 
