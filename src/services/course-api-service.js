f0c5813ca372c84332ecfc05f78322e2c8dd64f4
Daniel Fong <daniel.fong.mv@gmail.com>
2020-10-05 12:58:30 -0600

    added course API service

---
+++ src/services/course-api-service.js
@@ -0,0 +1,10 @@
+import config from '../config';
+import TokenService from './token-service';
+
+const CourseApiService = {
+    getCourses() {
+        return fetch()
+    }
+}
+
+export default CourseApiService;
