8e8c2b46a4629119a8d0b7254c917cabe6568ccb
Daniel Fong <daniel.fong.mv@gmail.com>
2020-10-05 12:41:51 -0600

    changed to course search

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,8 +4,8 @@ export default class Dashboard extends Component {
     render() {
         return (
             <div>
-                <label for='location-search'>Search Resorts and Locations</label>
-                <input class='location-search' name='location-search'>
+                <label for='course-search'>Search Courses</label>
+                <input class='course-search' name='course-search'>
                 </input>
             </div>
         )
