4d6a10355ea518038bbcfa6291d4043e1a8b24e2
Daniel Fong <daniel.fong.mv@gmail.com>
2020-10-06 11:44:46 -0600

    added login service for PDGA

--- src/services/course-api-service.js
+++ src/services/course-api-service.js
@@ -2,6 +2,19 @@ import config from '../config';
 import TokenService from './token-service';
 
 const CourseApiService = {
+    loginToSession() {
+        return fetch('https://api.pdga.com/services/json/user/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: {
+                "username":"Daniel Fong","password":"iWXjpfDQt5!deE@"
+            }
+        }).then((res) =>
+            !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+        );
+    },
     getCourses() {
         return fetch()
     }
