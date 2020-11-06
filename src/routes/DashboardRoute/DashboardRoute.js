c299ed80d1a9c5943ee9998a0cf659f063faf44b
Daniel Fong <daniel.fong.mv@gmail.com>
2020-11-06 11:44:44 -0700

    deleted course service api

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
 import Dashboard from '../../components/Dashboard/Dashboard';
-import CourseApiService from '../../services/course-api-service';
 
 export class DashboardRoute extends Component {
 
     state = {
-        sessionId: ''
+        
     };
 
     componentDidMount() {
-        // this.startSession();
-        this.searchCourses();
-    }
 
-    async startSession() {
-        const response = await CourseApiService.loginToSession();
-        console.log(response);
-    }
-
-    async searchCourses() {
-        const courses = await CourseApiService.getCourses();
-        console.log(courses)
     }
 
     render() {
